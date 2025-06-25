162c32cd8ea3173f8cb6e01aa767b4bba34ba7a2
cameronldn <me@cldn.dev>
2025-06-26 00:50:39 +1000

    Added a basic example of a question and answer for testing

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ const choices = ["Earth", "Mars", "Jupiter", "Saturn"];
 const correctAnswer = "Mars";
 
 function checkAnswer(button) {
-
+  if (button.value === correctAnswer) {
+    document.getElementById("result").innerHTML = "Correct!";
+  } else {
+    document.getElementById("result").innerHTML = "Wrong!";
+  }
+  for (let i = 0; i < choices.length; i++) {
+    document.getElementById(`choice${i + 1}`).disabled = true;
+  }
 }
 
 function displayQuestion() {
   let question_element = document.getElementById("question");
   question_element.innerHTML = question;
+  for (let i = 0; i < choices.length; i++) {
+    const btn = document.getElementById(`choice${i + 1}`);
+    btn.innerHTML = choices[i];
+    btn.value = choices[i];
+  }
 }
 
 displayQuestion();
